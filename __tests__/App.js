cefc28f858aecaf9cc5a87affd8688fe460a05d9
kiki le singe <aalbertini95@gmail.com>
2016-10-17 13:45:19 +0200

    test(App): fix tests

--- __tests__/App.js
+++ __tests__/App.js
@@ -3,10 +3,10 @@ import React from 'react'
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
-import Index from '../index.android'
+import App from '../src/App'
 
 it('renders correctly', () => {
   renderer.create(
-    <Index />
+    <App />
   )
 })
