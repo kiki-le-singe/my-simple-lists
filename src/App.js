3d4e7ccaeca40d84f45e64817df470f959385f01
kiki le singe <aalbertini95@gmail.com>
2016-11-23 14:40:53 +0100

    feat(App): implement Router

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  Actions,
+  Router,
+  Scene,
+} from 'react-native-router-flux'
+
+import CategoryList from './scenes/CategoryList'
+import Category from './scenes/Category'
+import NavigationDrawer from './components/NavigationDrawer'
+import LeftIcon from './components/LeftIcon'
+import layoutStyles from './styles/layout'
+
+const { titleStyle, navigationBarStyle, leftButtonIconStyle } = layoutStyles
+const leftIcon = () => (
+  <LeftIcon
+    name="md-menu"
+    onPress={() => Actions.refresh({ key: 'drawer', open: value => !value })}
+  />
+)
+
+export default function App() {
+  return (
+    <Router>
+      <Scene key="root">
+        <Scene key="drawer" component={NavigationDrawer} open={false}>
+          <Scene key="main">
+            <Scene
+              initial
+              key="categoryList"
+              renderLeftButton={leftIcon}
+              component={CategoryList}
+              title="Category List"
+              titleStyle={titleStyle}
+              navigationBarStyle={navigationBarStyle}
+              leftButtonIconStyle={leftButtonIconStyle}
+            />
+            <Scene
+              key="category"
+              component={Category}
+              title="Category"
+              titleStyle={titleStyle}
+              navigationBarStyle={navigationBarStyle}
+              leftButtonIconStyle={leftButtonIconStyle}
+            />
+          </Scene>
+        </Scene>
+      </Scene>
+    </Router>
+  )
+}
